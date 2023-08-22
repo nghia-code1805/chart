f66fd5ac35077456f6d2320e45c11f59a62d53b7
nghia_nun <nghiant@itsol.vn>
2023-08-22 22:53:31 +0700

    chart 3

--- resources/views/chart3.blade.php
+++ resources/views/chart3.blade.php
@@ -1,267 +1,69 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
 
 <head>
-	<script>
-		window.onload = function() {
-			var chart = new CanvasJS.Chart("chartContainer", {
-				title: {
-					text: "Weekly Revenue Analysis for First Quarter"
-				},
-				axisY: [{
-						title: "Order",
-						lineColor: "#C24642",
-						tickColor: "#C24642",
-						labelFontColor: "#C24642",
-						titleFontColor: "#C24642",
-						includeZero: true
-					},
-					{
-						title: "Footfall",
-						lineColor: "#369EAD",
-						tickColor: "#369EAD",
-						labelFontColor: "#369EAD",
-						titleFontColor: "#369EAD",
-						includeZero: true
-					},
-					{
-						title: "Revenue",
-						lineColor: "#7F6084",
-						tickColor: "#7F6084",
-						labelFontColor: "#7F6084",
-						titleFontColor: "#7F6084",
-						includeZero: true
-					}
-				],
-				// axisY2: {
-				// 	title: "Revenue",
-				// 	lineColor: "#7F6084",
-				// 	tickColor: "#7F6084",
-				// 	labelFontColor: "#7F6084",
-				// 	titleFontColor: "#7F6084",
-				// 	includeZero: true
-				// },
-				toolTip: {
-					shared: true
-				},
-				legend: {
-					cursor: "pointer",
-					itemclick: toggleDataSeries
-				},
-				data: [{
-						type: "line",
-						name: "Footfall",
-						color: "#369EAD",
-						showInLegend: true,
-						axisYIndex: 1,
-						dataPoints: [{
-								x: new Date(2017, 00, 7),
-								y: 85.4
-							},
-							{
-								x: new Date(2017, 00, 14),
-								y: 92.7
-							},
-							{
-								x: new Date(2017, 00, 21),
-								y: 64.9
-							},
-							{
-								x: new Date(2017, 00, 28),
-								y: 58.0
-							},
-							{
-								x: new Date(2017, 01, 4),
-								y: 63.4
-							},
-							{
-								x: new Date(2017, 01, 11),
-								y: 69.9
-							},
-							{
-								x: new Date(2017, 01, 18),
-								y: 88.9
-							},
-							{
-								x: new Date(2017, 01, 25),
-								y: 66.3
-							},
-							{
-								x: new Date(2017, 02, 4),
-								y: 82.7
-							},
-							{
-								x: new Date(2017, 02, 11),
-								y: 60.2
-							},
-							{
-								x: new Date(2017, 02, 18),
-								y: 87.3
-							},
-							{
-								x: new Date(2017, 02, 25),
-								y: 98.5
-							}
-						]
-					},
-					{
-						type: "line",
-						name: "Order",
-						color: "#C24642",
-						axisYIndex: 0,
-						showInLegend: true,
-						dataPoints: [{
-								x: new Date(2017, 00, 7),
-								y: 32.3
-							},
-							{
-								x: new Date(2017, 00, 14),
-								y: 33.9
-							},
-							{
-								x: new Date(2017, 00, 21),
-								y: 26.0
-							},
-							{
-								x: new Date(2017, 00, 28),
-								y: 15.8
-							},
-							{
-								x: new Date(2017, 01, 4),
-								y: 18.6
-							},
-							{
-								x: new Date(2017, 01, 11),
-								y: 34.6
-							},
-							{
-								x: new Date(2017, 01, 18),
-								y: 317.7
-							},
-							{
-								x: new Date(2017, 01, 25),
-								y: 24.7
-							},
-							{
-								x: new Date(2017, 02, 4),
-								y: 35.9
-							},
-							{
-								x: new Date(2017, 02, 11),
-								y: 12.8
-							},
-							{
-								x: new Date(2017, 02, 18),
-								y: 38.1
-							},
-							{
-								x: new Date(2017, 02, 25),
-								y: 42.4
-							}
-						]
-					},
-					{
-						type: "line",
-						name: "Revenue",
-						color: "#7F6084",
-						axisYIndex: 2,
-						showInLegend: true,
-						dataPoints: [{
-								x: new Date(2017, 00, 7),
-								y: 42.5
-							},
-							{
-								x: new Date(2017, 00, 14),
-								y: 44.3
-							},
-							{
-								x: new Date(2017, 00, 21),
-								y: 28.7
-							},
-							{
-								x: new Date(2017, 00, 28),
-								y: 22.5
-							},
-							{
-								x: new Date(2017, 01, 4),
-								y: 25.6
-							},
-							{
-								x: new Date(2017, 01, 11),
-								y: 45.7
-							},
-							{
-								x: new Date(2017, 01, 18),
-								y: 54.6
-							},
-							{
-								x: new Date(2017, 01, 25),
-								y: 32.0
-							},
-							{
-								x: new Date(2017, 02, 4),
-								y: 43.9
-							},
-							{
-								x: new Date(2017, 02, 11),
-								y: 26.4
-							},
-							{
-								x: new Date(2017, 02, 18),
-								y: 40.3
-							},
-							{
-								x: new Date(2017, 02, 25),
-								y: 54.2
-							}
-						]
-					}
-				]
-			});
-			chart.render();
-
-			var data = chart.options.data;
-
-			var dataPoints = [];
-			for (var i = 0; i < data.length; i++) {
-				dataPoints = data[i].dataPoints;
-
-				console.log(dataPoints);
-			}
-
-			var myChart = new Chart(chart, {
-				date: x,
-				data: y
-			});
-
-			console.log(myChart)
-
-			function toggleDataSeries(e) {
-				if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-					e.dataSeries.visible = false;
-				} else {
-					e.dataSeries.visible = true;
-				}
-				e.chart.render();
-			}
-
-		}
-
-	</script>
+	<title>Multi-Axis Line Chart</title>
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
 <body>
-	<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-	<table id="tableData">
-		<tr>
-			<td> date</td>
-			<td> CMI</td>
-			<td> KG</td>
-			<td> CM</td>
-		</tr>
-	</table>
+	<canvas id="myChart" width="400" height="100"></canvas>
+	<script>
+		var ctx = document.getElementById('myChart').getContext('2d');
 
-	<script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+		// Sample data for two datasets
+		var data = {
+			labels: ['January', 'February', 'March', 'April', 'May'],
+			datasets: [{
+					label: 'Dataset 1',
+					data: [10, 20, 15, 25, 30],
+					yAxisID: 'y-axis-1', // Associate with the first y-axis
+					borderColor: 'red',
+					backgroundColor: 'rgba(255, 0, 0, 0.2)',
+					fill: false
+				},
+				{
+					label: 'Dataset 2',
+					data: [15, 12, 11, 13, 14],
+					yAxisID: 'y-axis-2', // Associate with the second y-axis
+					borderColor: 'blue',
+					backgroundColor: 'rgba(0, 0, 255, 0.2)',
+					fill: false
+				}
+			]
+		};
+
+		// Define multiple y-axes
+		var options = {
+			scales: {
+				yAxes: [{
+						type: 'linear',
+						id: 'y-axis-1',
+						display: true,
+						position: 'left',
+						ticks: {
+							beginAtZero: true
+						}
+					},
+					{
+						type: 'linear',
+						display: true,
+						id: 'y-axis-2',
+						position: 'left',
+						ticks: {
+							beginAtZero: true
+						}
+					}
+				]
+			}
+		};
+
+		var myChart = new Chart(ctx, {
+			type: 'line',
+			data: data,
+			options: options
+		});
+	</script>
 </body>
 
 </html>
